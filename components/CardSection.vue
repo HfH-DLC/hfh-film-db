71401b9e800b56f71b8f7237886f69a9988d23bb
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-06-08 09:13:57 +0200

    Create CardSection.vue

---
+++ components/CardSection.vue
@@ -0,0 +1,35 @@
+<template>
+  <div v-if="content">
+    <div
+      class="
+        uppercase
+        text-xs
+        font-bold
+        text-thunderbird-red
+        group-hover:text-white
+        group-focus-visible:text-white
+      "
+    >
+      {{ label }}
+    </div>
+    <div v-html="content" />
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+    content: {
+      type: String,
+      default: "",
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
