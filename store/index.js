608dea9e93cf73eb206be101e39c196541f18bbb
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-06-08 09:15:53 +0200

    Update index.js

--- store/index.js
+++ store/index.js
@@ -1,40 +1,87 @@
 export const state = () => ({
     clips: [],
-    allClipsFetched: false
+    filters: [],
+    loading: false
 })
 
 export const mutations = {
-    addClip(state, clip) {
-        state.clips.push(clip);
-    },
     setClips(state, clips) {
         state.clips = clips
-        state.allClipsFetched = true
+    },
+    setFilters(state, filters) {
+        state.filters = filters
+    },
+    setFilter(state, { field, value }) {
+        const filter = state.filters.find(filter => filter.field === field);
+        if (filter) {
+            filter.value = value;
+        } else {
+            console.log(`Error: Filter ${field} not found.`)
+        }
+    },
+    setLoading(state, value) {
+        state.loading = value
     }
 }
 
 export const actions = {
-    async fetchClips({ state, commit }, params) {
-        if (state.allClipsFetched) {
-            return
+    async fetchClips({ commit }, params) {
+        commit('setLoading', true)
+        try  {
+            const response = await this.$axios.get(
+                `/api/clips/`, { params }
+            );
+            commit('setClips', response.data.records)
+        } catch (error) {
+            //todo error handling
+            console.log(error)
+        } finally {
+            commit('setLoading', false)
         }
-        const response = await this.$axios.get(
-            `/api/clips/`, { params }
-        );
-        commit('setClips', response.data.records)
 
     },
     async fetchClip({ state, commit }, id) {
-        const clip = state.clips[id];
+        const clip = state.clips.find(clip => clip.id === id);
         if (clip) {
             return
         }
-        //todo error handling
-        const response = await this.$axios.get(
-            `/api/clips/${id}`
-        );
-        commit('addClip', response.data)
-    }
+        commit('setLoading', true)
+        try {
+            const response = await this.$axios.get(
+                `/api/clips/${id}`
+            );
+            commit('setClips', [response.data])
+        } catch (error) {
+            //todo error handling
+            console.log(error)
+        } finally {
+            commit('setLoading', false)
+        }
+    },
+    async fetchFilters({ commit }, params) {
+        commit('setLoading', true)
+        try  {
+            const response = await this.$axios.get(
+                `/api/clips/fields`, { params }
+            );
+            const filters = Object.entries(response.data).map(([key, values]) => {
+                return {
+                    field: key,
+                    label: key,
+                    options: values.map(fieldValue => { return { label: fieldValue, value: fieldValue } }),
+                    value: ""
+                }
+            })
+
+            commit('setFilters', filters)
+        } catch (error) {
+            //todo error handling
+            console.log(error)
+        } finally {
+            commit('setLoading', false)
+        }
+
+    },
 }
 
 export const getters = {
