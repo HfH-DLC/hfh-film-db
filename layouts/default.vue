d52ab2b8cca1373027633f2c38bfa26710a00c62
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-06-08 09:14:13 +0200

    Create default.vue

---
+++ layouts/default.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="bg-white h-full flex flex-col">
+    <h1 class="text-2xl block px-8 pt-8 pb-4 text-thunderbird-red">
+      <nuxt-link to="/">Flimclips</nuxt-link>
+    </h1>
+    <Nuxt class="flex-1" />
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+</style>
