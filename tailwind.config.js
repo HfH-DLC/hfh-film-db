3d2ffc288db9414f6b9ae99920a52e05670473ac
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-06-08 09:16:00 +0200

    Update tailwind.config.js

--- tailwind.config.js
+++ tailwind.config.js
@@ -9,12 +9,26 @@ module.exports = {
     theme: {
         extend: {
             colors: {
-                'thunderbird-red': '#be1925'
+                'fantasy-light': '#fbf3f4',
+                'fantasy-plain': '#f2d1d3',
+                'fantasy-dark': '#d8757c',
+                'gray-dark': '#767676',
+                'gray-light': '#f4f4f4',
+                'thunderbird-red': '#be1925',
+                'thunderbird-red-light': '#e31826'
             },
             scale: {
                 '101': '1.01',
+            },
+            backgroundImage: {
+                'dropdown-arrow': "url('@/assets/images/dropdown-arrow.png')",
+                'search': "url('@/assets/images/search.svg')",
+                'search-active': "url('@/assets/images/search-active.svg')"
+            },
+            gridTemplateColumns: {
+                'index-xs': 'repeat(auto-fill, minmax(100%, 1fr))',
+                'index': 'repeat(auto-fill, minmax(400px, 1fr))',
             }
         },
     },
-    plugins: [],
 }
