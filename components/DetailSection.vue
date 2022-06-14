41fde189bbad0c22a87e6f033732dffc022aa16c
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-06-14 12:13:20 +0200

    Create DetailSection.vue

---
+++ components/DetailSection.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="mt-4">
+    <div
+      class="
+        uppercase
+        text-xs
+        font-bold
+        text-thunderbird-red
+        group-hover:text-white
+        group-focus-visible:text-white
+      "
+    >
+      {{ label }}
+    </div>
+    <div>
+      <slot />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  props: {
+    label: {
+      type: String,
+      required: true,
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+</style>
