579277361f8d2e324e85ee2cf2ca2f4ed980e077
Matthias Nötzli <matthias.noetzli@gmail.com>
2022-10-18 17:40:06 +0200

    Add caching

--- server/api/clips.get.js
+++ server/api/clips.get.js
@@ -1,4 +1,7 @@
 import helpers from "@/server/utils/airtable";
+import NodeCache from "node-cache";
+
+const cache = new NodeCache({ stdTTL: 300 });
 
 export default defineEventHandler(async (event) => {
   const query = useQuery(event);
@@ -14,17 +17,20 @@ export default defineEventHandler(async (event) => {
     }
   });
   const formula = `AND(${conditions.join(", ")})`;
-  const records = await helpers.getRecords("Clips", {
-    sort: [{ field: "Clip Nr.", direction: "asc" }],
-    filterByFormula: formula,
-  });
+  let records = cache.get(formula);
+  if (!records) {
+    records = await helpers.getRecords("Clips", {
+      sort: [{ field: "Clip Nr.", direction: "asc" }],
+      filterByFormula: formula,
+    });
+    cache.set(formula, records);
+  }
 
   const startIndex = (query.currentPage - 1) * query.pageSize;
   const endIndex = query.currentPage * query.pageSize;
 
   const currentPageRecords = records.slice(startIndex, endIndex);
   const totalPages = Math.ceil(records.length / query.pageSize);
-  console.log("totalPages", totalPages);
   return {
     records: currentPageRecords,
     pageSize: query.pageSize,
